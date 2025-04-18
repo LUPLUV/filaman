d4ec23780bb3dd9bc87bf5c0d165fd57dd3f673e
thore <thore.mueller07@gmail.com>
2025-04-18 16:39:50 +0200

    remove logto

--- lib/password.ts
+++ lib/password.ts
@@ -1,7 +1,3 @@
-import bcrypt from "bcrypt";
-
 export function saltAndHashPassword(password: string) {
-    const saltRounds = 10; // Number of salt rounds (higher is more secure but slower)
-    const salt = bcrypt.genSaltSync(saltRounds);
-    return bcrypt.hashSync(password, salt);
+    return password;
 }
