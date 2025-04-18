a1ae29cd2232cf6adcead962d17547658ba0c868
thore <thore.mueller07@gmail.com>
2025-04-18 16:36:45 +0200

    remove logto

--- app/(panel)/page.tsx
+++ app/(panel)/page.tsx
@@ -1,29 +1,9 @@
 import {FilamentOverview} from "@/app/(panel)/_components/filament-overview";
-import {getLogtoContext, signIn} from '@logto/next/server-actions';
-import {logtoConfig} from "@/lib/logto";
-import SignIn from "@/app/auth/login-form";
 
 export default async function Panel() {
-    const {isAuthenticated, claims} = await getLogtoContext(logtoConfig);
-
-    if (!isAuthenticated) {
-        return (
-            <div>
-                Not authenticated
-                <SignIn
-                    onSignIn={async () => {
-                        'use server';
-
-                        await signIn(logtoConfig);
-                    }}
-                />
-            </div>
-        )
-    }
 
     return (
         <div className="w-full min-h-screen">
-            Hello, {claims?.sub}
             <FilamentOverview/>
         </div>
     );
