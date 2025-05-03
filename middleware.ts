36f516b494ded63e50b611747724c7d3a2886e9c
thore <thore.mueller07@gmail.com>
2025-05-03 21:01:58 +0200

    fix protection

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,3 @@
-import {clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
-
-const isProtectedRoute = createRouteMatcher(['/(.*)']);
-
-export default clerkMiddleware(async (auth, req) => {
-    if (isProtectedRoute(req)) await auth.protect()
-})
-
 export const config = {
     matcher: [
         // Skip Next.js internals and all static files, unless found in search params
